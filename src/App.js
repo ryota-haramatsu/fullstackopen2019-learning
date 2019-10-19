5ae40ac2ecf2322b268e75e15263f5577fcb7662
リョウタ <haramatsuryota@gmail.com>
2019-10-19 21:42:29 +0800

    add part2-b

--- src/App.js
+++ src/App.js
@@ -1,38 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './App.css';
-import Course from './components/Course';
+
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: "080-1111-1111" },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  
+  const newObject = {
+    id: persons.length + 1,
+    name: newName,
+    number: newNumber
   }
 
+  const addList = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat(newObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const NumbersList = () => {
+    const list = persons.map((value, key) => {
+        return <div key={key}> {`${persons[key].name} ${persons[key].number}`}</div>
+      })
+    return (
+      <div>
+        {list}
+      </div>
+    )
+  }
+
+  const handleNameChange = (event) => {
+    persons.map((value, key) => {
+      if (event.target.value === persons[key].name) {        
+        alert(`${newName} is already added to phonebook`)
+    }
+    })  
+    setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    persons.map((value, key) => {
+      if (event.target.value === persons[key].number) {
+        alert(`${newNumber} is already added to phonebook`)
+      }
+    })
+    setNewNumber(event.target.value);
+  }
+ 
   return (
     <div>
-      <Course course={course} />
+      <div>
+        <h2>add a new</h2>
+        <form onSubmit={addList}>
+          <div>
+            name:
+            <input
+              value={newName}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div>
+            number:
+            <input
+              value={newNumber}
+              onChange={handleNumberChange}
+            />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
+
+      <div>
+        <h2>Numbers</h2>
+        <NumbersList />
+      </div>
     </div>
   )
+
 }
+
 export default App;
